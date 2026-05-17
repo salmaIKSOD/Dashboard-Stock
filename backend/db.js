55ede54cf8e23216cec8b838e4754e06c089890f
salmaIKSOD <www.salmalak2003@gmail.com>
2026-05-18 00:34:40 +0100

    update backend

--- backend/db.js
+++ backend/db.js
@@ -1,38 +1,3 @@
-// const sql = require('mssql');
-// require('dotenv').config();
-// // require('dotenv').config({ path: './API/.env' });
-
-// const config = {
-//   server: process.env.DB_SERVER,
-//   database: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   port: parseInt(process.env.DB_PORT) || 1433,
-//   options: {
-//     encrypt: false,
-//     trustServerCertificate: true,
-//     enableArithAbort: true,
-//   },
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000,
-//   },
-// };
-
-// let pool = null;
-
-// async function getPool() {
-//   if (!pool) {
-//     pool = await sql.connect(config);
-//     console.log('✅ Connecté à SQL Server —', process.env.DB_NAME);
-//   }
-//   return pool;
-// }
-
-// module.exports = { getPool, sql };
-
-
 const sql = require('mssql');
 
 const config = {
@@ -64,28 +29,3 @@ async function getPool() {
 }
 
 module.exports = { getPool, sql };
-// ancienn db.js
-// const sql = require('mssql');
-// require('dotenv').config();
-
-// const config = {
-//   server: process.env.DB_SERVER,
-//   database: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   port: parseInt(process.env.DB_PORT),
-//   requestTimeout: 300000,   // ← 120 secondes au lieu de 15
-//   connectionTimeout: 30000,
-//   options: {
-//     encrypt: false,
-//     trustServerCertificate: true,
-//     enableArithAbort: true,
-//   }
-// };
-
-// const poolPromise = new sql.ConnectionPool(config)
-//   .connect()
-//   .then(pool => { console.log('Connecté à SQL Server ✅'); return pool; })
-//   .catch(err => console.error('Erreur connexion DB:', err));
-
-// module.exports = { sql, poolPromise };
