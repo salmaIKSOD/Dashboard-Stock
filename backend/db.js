8c4df8ed27f7cde28a7dc34737df605318073cd3
salmaIKSOD <www.salmalak2003@gmail.com>
2026-05-18 12:58:22 +0100

    cacher les données env dans db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,17 @@
 const sql = require('mssql');
+require('dotenv').config({ path: '../.env' });
 
 const config = {
-  server: 'SALMAIKSOD\\SAGE100',
-  database: 'StockAnalytics',
-  user: 'sa',
-  password: '123456',
-  port: 1433,
-  // ajouter deux ligne
-  requestTimeout:    300000,   // 5 minutes par requête
-  connectionTimeout:  30000,   // 30 secondes pour la connexion
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+
+  //  deux 
+  requestTimeout:    300000,   
+  connectionTimeout:  30000,   
+  
   options: {
     encrypt: false,
     trustServerCertificate: true,
